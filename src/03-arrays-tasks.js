77b5b69c38a17c3bd9ed5ebeaf78ea69d5cbd658
k98940 <pv1xxb@yandex.ru>
2023-09-13 16:35:58 +0600

    feat: swapHeadAndTail

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -601,10 +601,14 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
-}
+function swapHeadAndTail(arr) {
+  const middle = Math.floor(arr.length / 2);
+  const head = arr.splice(0, middle);
+  const tail = arr.splice(-middle);
 
+  tail.push(...arr, ...head);
+  return tail;
+}
 
 module.exports = {
   findElement,
