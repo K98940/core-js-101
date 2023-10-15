d9164cba2ec01184c5e4abcf1170262df18ab1c3
k98940 <pv1xxb@yandex.ru>
2023-10-15 16:22:06 +0600

    feat: getLinearEquationRoot and getAngleBetweenVectors are ready. (Sun, Oct 15, 2023 04:22:06 PM)

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -86,8 +86,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return (b * -1) / a;
 }
 
 
@@ -109,8 +109,12 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const skalar = (x1 * x2) + (y1 * y2);
+  const moduleA = Math.sqrt(x1 * x1 + y1 * y1);
+  const moduleB = Math.sqrt(x2 * x2 + y2 * y2);
+  const cos = Math.acos(skalar / (moduleA * moduleB));
+  return cos;
 }
 
 /**
